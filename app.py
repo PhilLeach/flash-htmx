6e4f1d6e265668726a2b0be7fbd1c24d17f1b2e2
Philip Leach <phil@pa-leach.com>
2023-10-15 17:24:57 +0100

    merge

--- app.py
+++ app.py
@@ -1,21 +1,12 @@
 # app.py
 
 from flask import Flask, render_template, request
-from flask_assets import Bundle, Environment
 
 from todo import todos
 
 
 app = Flask(__name__)
 
-assets = Environment(app)
-css = Bundle("src/main.css", output="dist/main.css")
-js = Bundle("src/*.js", output="dist/main.js") # new
-
-assets.register("css", css)
-assets.register("js", js) # new
-css.build()
-js.build() # new
 
 
 @app.route("/")
@@ -38,5 +29,3 @@ def search_todo():
     return render_template("todo.html", todos=res_todos)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
